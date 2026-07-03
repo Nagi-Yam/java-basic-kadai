79c09d462eb6f6ffda60b682668c7010ed993e97
Nagi-Yam <nagi.yamanaka@y-yokohama.com>
2026-07-03 14:46:15 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,35 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	private int gear = 1;
+	private int speed = 10;
+	
+	public	void changeGear(int afterGear) {
+		int beforGear = this.gear;
+		this.gear = afterGear ;
+		this.speed = switch(afterGear) {
+		
+		case 1 -> 10;
+		case 2 -> 20;
+		case 3 -> 30;
+		case 4 -> 40;
+		case 5 -> 50;
+		default -> 10;
+		
+		};
+		
+		System.out.println("ギア"+beforGear+"から" + this.gear + "に切り替えました");
+		
+	}
+		public void run() {
+			
+			
+			System.out.println("速度は時速"+this.speed);
+		}
+	
+		
+	
+	}
+
+
